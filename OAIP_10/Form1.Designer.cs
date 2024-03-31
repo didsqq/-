4a5b391f56b33977d1923427c29a9d2322c80f50
adel <adelgaripov6@gmail.com>
2024-03-31 18:35:07 +0300

    инициализирую гит

---
+++ OAIP_10/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace OAIP_10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 82);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(217, 35);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Начать сортировку";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(248, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(839, 529);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(230, 127);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Выбрать метод сортировки";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.radioButton1.Location = new System.Drawing.Point(6, 22);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(210, 24);
+            this.radioButton1.TabIndex = 1;
+            this.radioButton1.Text = "Сортировка выбором";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.radioButton2.Location = new System.Drawing.Point(6, 52);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(206, 24);
+            this.radioButton2.TabIndex = 2;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Быстрая сортировка";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1099, 553);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+    }
+}
+
---
+++ OAIP_10/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OAIP_10
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Sort();
+        }
+
+        private void Sort(){
+            if (radioButton1.Checked == true)
+            {
+                int[] mas = GenerateArray(10, 1, 100);
+                mas = ViborSort(mas);
+
+                richTextBox1.Text = "";
+                for (int i = 0; i < mas.Length; i++)
+                {
+                    if (i != mas.Length - 1)
+                        richTextBox1.Text += mas[i] + " ";
+                    else
+                        richTextBox1.Text += mas[i];
+                }
+
+            }
+            else if (radioButton2.Checked == true)
+            {
+                int[] mas = GenerateArray(10, 1, 100);
+                mas = QuickSort(mas, 0, mas.Length - 1);
+
+                richTextBox1.Text = "";
+                for (int i = 0; i < mas.Length; i++)
+                {
+                    if (i != mas.Length - 1)
+                        richTextBox1.Text += mas[i] + " ";
+                    else
+                        richTextBox1.Text += mas[i];
+                }
+            }
+            else
+            {
+                MessageBox.Show("Выберите метод");
+            }
+        }
+        static int[] GenerateArray(int length, int minValue, int maxValue)
+        {
+            int[] array = new int[length];
+            Random rand = new Random();
+
+            for (int i = 0; i < length; i++)
+            {
+                array[i] = rand.Next(minValue, maxValue + 1);
+            }
+
+            return array;
+        }
+
+        static int[] ViborSort(int[] mas)
+        {
+            for (int i = 0; i < mas.Length - 1; i++)
+            {
+                for(int j = 0; j < mas.Length - 1; j++)
+                {
+                    richTextBox1.Text += mas[i];
+                }
+                //поиск минимального числа
+                int min = i;
+                for (int j = i + 1; j < mas.Length; j++)
+                {
+                    if (mas[j] < mas[min])
+                    {
+                        min = j;
+                    }
+                }
+                //обмен элементов
+                int temp = mas[min];
+                mas[min] = mas[i];
+                mas[i] = temp;
+            }
+            return mas;
+        }
+
+        static int[] QuickSort(int[] array, int a, int b)
+        {
+            int i = a;
+            int j = b;
+            int middle = array[(a + b) / 2];
+            while (i <= j)
+            {
+                while (array[i] < middle)
+                {
+                    i++;
+                }
+                while (array[j] > middle)
+                {
+                    j--;
+                }
+                if (i <= j)
+                {
+                    int temporaryVariable = array[i];
+                    array[i] = array[j];
+                    array[j] = temporaryVariable;
+                    i++;
+                    j--;
+                }
+            }
+            if (a < j)
+            {
+                QuickSort(array, a, j);
+            }
+            if (i < b)
+            {
+                QuickSort(array, i, b);
+            }
+            return array;
+        }
+    }
+}
