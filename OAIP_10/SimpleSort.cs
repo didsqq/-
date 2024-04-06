d012195fcf3b1bd264efe8c6b1a94573151865c7
adel <adelgaripov6@gmail.com>
2024-04-06 14:36:15 +0300

    импорт экспорт

--- OAIP_10/SimpleSort.cs
+++ OAIP_10/SimpleSort.cs
@@ -21,52 +21,21 @@ namespace OAIP_10
                 int min = i;
                 for (int j = i + 1; j < mas.Length; j++)
                 {
-                    if (mas[j] < mas[min] && Comprassions(mas, j, min, flag))
+                    if (mas[j] < mas[min] && Swap_Comp.Comprassions(mas, j, min, flag, comprassions))
                     {
                         min = j;
+                        comprassions++;
                     }
                 }
 
-                Swap(mas, min,i, flag);
+                Swap_Comp.Swap(mas, min,i, flag, permutations);
+                permutations++;
             }
 
             sw.Stop();
             var resultTime = sw.Elapsed;
             string time = String.Format("{0:00}:{1:00}.{2:000}", resultTime.Minutes, resultTime.Seconds, resultTime.Milliseconds);
             Form1.ReadInfo(comprassions, permutations, time);
-        }
-
-        private void Swap(int[] mas, int i1, int i2, bool flag)
-        {
-            permutations++;
-            //if (flag)
-            //FileOut.filestring += $"Перемещение {permutations}: " + $"[{i1}] - {mas[i1]} и [{i2}] - {mas[i2]}\n";
-
-            string swapString = "";
-            for (int i = 0; i < mas.Length; i++)
-                if (i == i1 || i == i2)
-                    swapString += $"[{mas[i]}]";
-                else
-                    swapString += mas[i] + " ";
-            Form1.AddSortLine(swapString);
-
-            int temp = mas[i1];
-            mas[i1] = mas[i2];
-            mas[i2] = temp;
-        }
-
-        private bool Comprassions(int[] arr, int ind1, int ind2, bool flag)
-        {
-            comprassions++;
-
-            if(flag && FileOut.fileString == null)
-            {
-                FileOut.fileString = "Исходный массив: ";
-                FileOut.Fill();
-            }
-            if (flag)
-                FileOut.fileString += $"Сравнение {comprassions}: " + $"{arr[ind1]} и {arr[ind2]}\n";
-            return true;
         }
     }
 }
---
+++ OAIP_10/Swap_Comp.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OAIP_10
+{
+    static class Swap_Comp
+    {
+        public static void Swap(int[] mas, int i1, int i2, bool flag, int permutations)
+        {
+            permutations++;
+            if (flag)
+                FileOut.fileString += $"Перемещение {permutations}: " + $"[{i1}] - {mas[i1]} и [{i2}] - {mas[i2]}\n";
+
+            string swapString = "";
+            for (int i = 0; i < mas.Length; i++)
+                if (i == i1 || i == i2)
+                    swapString += $"[{mas[i]}]";
+                else
+                    swapString += mas[i] + " ";
+            Form1.AddSortLine(swapString);
+
+            int temp = mas[i1];
+            mas[i1] = mas[i2];
+            mas[i2] = temp;
+        }
+
+        public static bool Comprassions(int[] arr, int ind1, int ind2, bool flag, int comprassions)
+        {
+            comprassions++;
+            if (flag && FileOut.fileString == null)
+            {
+                FileOut.fileString = "Исходный массив: ";
+                FileOut.Fill();
+            }
+            if (flag)
+                FileOut.fileString += $"Сравнение {comprassions}: " + $"{arr[ind1]} и {arr[ind2]}\n";
+            return true;
+        }
+    }
+}
