974be71eb6e024a086026c70c9a1c7f3ab242c48
adelgaripov <adelgaripov6@gmail.com>
2024-04-08 21:29:22 +0300

    быстрая сортировка работает медленно и пишет фигню в txt

--- OAIP_10/ImprovedSort.cs
+++ OAIP_10/ImprovedSort.cs
@@ -14,6 +14,7 @@ namespace OAIP_10
         public void SortArr(int[] array, bool flag)
         {
             System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+            sw.Start();
 
             FastSortArr(array, 0, array.Length - 1,flag);
 
@@ -21,6 +22,11 @@ namespace OAIP_10
             var resultTime = sw.Elapsed;
             string time = String.Format("{0:00}:{1:00}.{2:000}", resultTime.Minutes, resultTime.Seconds, resultTime.Milliseconds);
             Form1.ReadInfo(comprassions, permutations, time);
+
+            string sortString = "";
+            for (int k = 0; k < array.Length; k++)
+                sortString += array[k] + " ";
+            Form1.AddSortLine(sortString);
         }
 
         public void FastSortArr(int[] array,int a, int b, bool flag)
@@ -30,20 +36,22 @@ namespace OAIP_10
             int middle = array[(a + b) / 2];
             while (i <= j)
             {
-                while (array[i] < middle)
+                while (Swap_Comp.Comprassions(array, i, (a + b) / 2, flag, comprassions))//array[i] < middle
                 {
                     i++;
+                    comprassions++;
                 }
-                while (array[j] > middle)
+                while (Swap_Comp.Comprassions(array, (a + b) / 2, j, flag, comprassions))//array[j] > middle
                 {
                     j--;
+                    comprassions++;
                 }
                 if (i <= j)
                 {
                     Swap_Comp.Swap(array, i, j, flag, permutations);
+                    permutations++;
                     i++;
                     j--;
-                    permutations++;
                 }
             }
             if (a < j)
